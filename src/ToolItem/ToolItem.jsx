edc201b051097f12b0617ee4240b4c0a2ba9ffd5
mike <you@example.com>
2021-03-01 14:50:59 -0500

    messing with state

---
+++ src/ToolItem/ToolItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ToolItem = (props) => {
+return (
+    <tr className="">
+    <td className="">John</td>
+    <td className="">Approved</td>
+    <td className="right aligned">None</td>
+</tr>
+)
+}
--- src/ViewTool/ViewTools.jsx
+++ src/ViewTool/ViewTools.jsx
@@ -1,28 +1,42 @@
 import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
 
-
+import ToolItem from '../ToolItem';
 import { toolActions } from '../_actions';
 
 const ViewTools = (props) => {
     const [toolState ,setTools] = useState({})
     useEffect(() => {
-       props.getAll()
-       setTools(props.tools)
-        console.log(toolState)
-    }, [toolState])
+       setTools(props.getAll())
+      
+        console.log(`toolState: ${props.tools}`)
+    }, [])
+    console.log("props")
+console.log(props)
     return (
-    <div>
-
-    <div>View Toolz</div>
-   {/*  {toolState ? toolState : ''} */}
-    </div>
+        <table className="ui celled inverted selectable table">
+            <thead className="">
+                <tr className="">
+                    <th className="">Id</th>
+                    <th className="">Tool Name</th>
+                    <th className="">Description</th>
+                    <th className="">Showname</th>
+                    <th className="">Last User</th>
+                    <th className="">Last User Sign out</th>
+                    <th className="">Current User</th>
+                    <th className="">Current User Sign out</th>
+                </tr></thead>
+                <tbody className="">
+               
+                </tbody>
+            </table>
      
     )
 }
+
 const mapStateToProps = (state) => {
-    const {tools} = state.tools;
-    return {tools}
+    
+    return {tools: state.tools}
 }
 const actionCreators = {
     getAll: toolActions.getAll
