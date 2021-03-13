1df1a7b3a25f12c7ca3821e5cda105b40f33a034
mike <michael.a.t.jay@gmail.com>
2021-03-13 17:04:55 -0500

    added model

--- src/ToolItem/ToolItem.jsx
+++ src/ToolItem/ToolItem.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import { Dropdown, Menu } from 'semantic-ui-react';
 
 const ToolItem = (props) => {
-console.log("items")
-/* console.log(props.items) */
 
+const executeAction = (e , {value}) => {
+
+    switch (value){
+        case "edit":
+            //todo: pop up modal for edit, then call redux action for update
+            console.log("edit called");
+           props.setEditModal(!props.editModal)
+            break;
+        case "delete":
+            //todo pop up modal asking for permission to delete, redux action on confirm
+            console.log("delete called");
+            break;
+        case "reminder":
+            //pop up modal with reminder email options then redux send
+            console.log("reminder called")
+            break;
+        default:
+            break;
+    }
+}
+
+const actionOption = [
+    {
+        key:"edit",
+        text:"Edit",
+        value:"edit"
+        // add wrench image for edit
+    },
+    {
+        key:"delete",
+        text:"Delete",
+        value:"delete"
+    },
+    {
+        key:"reminder",
+        text:"Send Reminder",
+        value:"reminder"
+    }
+]
 return (
 <tr className="">
     <td className="">{props.item.uid}</td>
@@ -14,6 +52,15 @@ return (
     <td className="">{props.item.signouttime}</td>
     <td className="">{props.item.currentuserid}</td>
     <td className="">{props.item.lastusersignout}</td>
+    <td>
+    
+    <Menu compact>
+    <Dropdown options={actionOption} simple item onChange={executeAction}/>
+  </Menu>
+
+
+    </td>
+    
 </tr> 
 )
 }
--- src/ViewTool/ViewTools.jsx
+++ src/ViewTool/ViewTools.jsx
@@ -4,10 +4,18 @@ import _ from 'lodash';
 import { alertActions } from '../_actions';
 import ToolItem from '../ToolItem/ToolItem';
 import { toolActions,getAll,clearTools } from '../_actions';
+import { Button, Icon, Modal,Form } from 'semantic-ui-react'
 
+
+const options = [
+    { key: '1', text: 'Test Show', value: 'test' },
+    { key: '2', text: 'Amazing Great Show', value: 'amazing great show' },
+    { key: '3', text: 'Wow great show', value: 'wow great show' },
+  ]
 const ViewTools = (props) => {
-
-   
+const [editModalOpen, setEditModalOpen] = useState(false);
+const [firstOpen, setFirstOpen] = React.useState(false)
+const [secondOpen, setSecondOpen] = React.useState(false)
     useEffect(() => {
         //this.props.clearAlerts();
        if(props.tools != undefined){
@@ -17,9 +25,82 @@ const ViewTools = (props) => {
        
     }, [])
 
-
+const openEditModal = (val) => {
+    setEditModalOpen(val)
+}
     return (
-        <table className="ui celled inverted selectable table">
+        <div>
+             <>
+     
+
+      <Modal
+        onClose={() => setFirstOpen(false)}
+        onOpen={() => setFirstOpen(true)}
+        open={editModalOpen}
+      >
+        <Modal.Header>Edit Record?</Modal.Header>
+        <Modal.Content image>
+          <div className='image'>
+            <Icon name='right arrow' />
+          </div>
+          <Modal.Description>
+            <p>Are you sure you want to edit record?</p>
+          </Modal.Description>
+        </Modal.Content>
+        <Modal.Actions>
+        <Button onClick={() => setEditModalOpen(false)} >
+            Cancel <Icon name='ban' />
+          </Button>
+          <Button onClick={() => setSecondOpen(true)} primary>
+            Proceed <Icon name='right chevron'/>
+          </Button>
+        </Modal.Actions>
+
+        <Modal
+          onClose={() => setSecondOpen(false)}
+          open={secondOpen}
+          size='small'
+        >
+          <Modal.Header>Edit Record []</Modal.Header>
+          <Modal.Content>
+          <Form >
+        <Form.Group widths='equal'>
+          <Form.Input fluid label='Tool Name' placeholder='Tool Name' />
+         
+          <Form.Select
+           
+            fluid
+            label='Show'
+            options={options}
+            placeholder='Show'
+          />
+         
+        </Form.Group>
+      
+        <Form.TextArea label='Description' placeholder='Tool Description...' />
+        
+        <Form.Button>Submit</Form.Button>
+      </Form>
+          </Modal.Content>
+          <Modal.Actions>
+          <Button
+              icon='ban'
+              content='Cancel'
+              onClick={() => setSecondOpen(false)}
+            />
+            <Button
+              icon='check'
+              content='All Done'
+              onClick={() =>{ 
+                  setSecondOpen(false)
+                  setEditModalOpen(false)
+                }}
+            />
+          </Modal.Actions>
+        </Modal>
+      </Modal>
+    </>
+            <table className="ui celled inverted selectable table">
             <thead className="">
                 <tr className="">
                     <th className="">Id</th>
@@ -30,15 +111,19 @@ const ViewTools = (props) => {
                     <th className="">Last User Sign out</th>
                     <th className="">Current User</th>
                     <th className="">Current User Sign out</th>
+                    <th>Actions</th>
+                    
                 </tr></thead>
                 <tbody className="">
               
                 {props.tools != undefined ? props.tools.map((item,index) => (
-                    <ToolItem item={item} key={index}/>
+                    <ToolItem editModal={editModalOpen} setEditModal={openEditModal} item={item} key={index}/>
                 )): ""}
                     
                 </tbody>
             </table>
+        </div>
+        
      
     )
 }
